af83a37d9628b8474b1a174623dd4b52b8d6a129
Alper Efe Şahin <efe.mate@gmail.com>
2025-02-09 15:20:20 +0300

    Update dependency_injector.dart

--- lib/core/di/dependency_injector.dart
+++ lib/core/di/dependency_injector.dart
@@ -1,10 +1,15 @@
 import 'package:get_it/get_it.dart';
 import 'package:mapmotion_flutter/core/interfaces/i_location_permission_service.dart';
+import 'package:mapmotion_flutter/core/interfaces/i_location_service.dart';
 import 'package:mapmotion_flutter/data/services/location_permission_service.dart';
+import 'package:mapmotion_flutter/data/services/location_service.dart';
 
 final getIt = GetIt.instance;
 
 void injectionSetup() {
   // Location Permission Service
   getIt.registerLazySingleton<ILocationPermissionService>(() => LocationPermissionService());
+
+  // Location Service
+  getIt.registerLazySingleton<ILocationService>(() => LocationService());
 }
